162348ec15cf98512a9d8ad6c249ed6edbe4b01d
Massimiliano Mirra <bard@hyperstruct.net>
2021-06-01 18:01:44 +0100

    dev,enh: convert assertions to pytest format

--- tests/test_cmd_generate_migration.py
+++ tests/test_cmd_generate_migration.py
@@ -12,14 +12,10 @@ class CmdGenerateMigrationsTestCase(BaseTestCase):
 
         cmd_generate_migration("schema.sql", "migrations")
 
-        self.assertTrue("Schema file name: schema.sql" in self.stdout.getvalue())
-        self.assertTrue("Migration directory: migrations" in self.stdout.getvalue())
-        self.assertTrue(
-            "Current schema fingerprint: 628c46f278dd3da2" in self.stdout.getvalue()
-        )
-        self.assertTrue(
-            "Last migrated schema fingerprint: none" in self.stdout.getvalue()
-        )
+        assert "Schema file name: schema.sql" in self.stdout.getvalue()
+        assert "Migration directory: migrations" in self.stdout.getvalue()
+        assert "Current schema fingerprint: 628c46f278dd3da2" in self.stdout.getvalue()
+        assert "Last migrated schema fingerprint: none" in self.stdout.getvalue()
 
     def test_generate_first_migration_when_none_present(self):
         with open("schema.sql", "w") as file:
@@ -27,16 +23,12 @@ class CmdGenerateMigrationsTestCase(BaseTestCase):
 
         cmd_generate_migration("schema.sql", "migrations")
 
-        self.assertTrue(
-            "Current schema fingerprint: 628c46f278dd3da2" in self.stdout.getvalue()
-        )
-        self.assertTrue(
-            "Last migrated schema fingerprint: none" in self.stdout.getvalue()
-        )
-        self.assertTrue(os.path.exists("migrations/000_none-628c46f278dd3da2.sql"))
+        assert "Current schema fingerprint: 628c46f278dd3da2" in self.stdout.getvalue()
+        assert "Last migrated schema fingerprint: none" in self.stdout.getvalue()
+        assert os.path.exists("migrations/000_none-628c46f278dd3da2.sql")
         with open("migrations/000_none-628c46f278dd3da2.sql") as migration_file:
             migration_content = migration_file.read()
-            self.assertTrue('create table "public"."users"' in migration_content)
+            assert 'create table "public"."users"' in migration_content
 
     def test_do_nothing_when_current_schema_already_matches_last_migration(self):
         with open("schema.sql", "w") as file:
@@ -45,7 +37,7 @@ class CmdGenerateMigrationsTestCase(BaseTestCase):
 
         cmd_generate_migration("schema.sql", "migrations")
 
-        self.assertTrue(
+        assert (
             "Schema and last migration match, nothing to do." in self.stdout.getvalue()
         )
 
@@ -56,7 +48,7 @@ class CmdGenerateMigrationsTestCase(BaseTestCase):
         self.repo.index.commit(".")
 
         cmd_generate_migration("schema.sql", "migrations")
-        self.assertTrue(os.path.exists("migrations/000_none-628c46f278dd3da2.sql"))
+        assert os.path.exists("migrations/000_none-628c46f278dd3da2.sql")
 
         with open("schema.sql", "a") as file:
             file.write("CREATE TABLE widgets (id INTEGER, name TEXT);\n")
@@ -69,9 +61,7 @@ class CmdGenerateMigrationsTestCase(BaseTestCase):
         self.repo.index.commit(".")
 
         cmd_generate_migration("schema.sql", "migrations")
-        self.assertTrue(
-            os.path.exists("migrations/001_628c46f278dd3da2-376ef48bf0288477.sql")
-        )
+        assert os.path.exists("migrations/001_628c46f278dd3da2-376ef48bf0288477.sql")
 
     def test_generate_destructive_migrations(self):
         with open("schema.sql", "w") as file:
@@ -80,7 +70,7 @@ class CmdGenerateMigrationsTestCase(BaseTestCase):
         self.repo.index.commit(".")
 
         cmd_generate_migration("schema.sql", "migrations")
-        self.assertTrue(os.path.exists("migrations/000_none-628c46f278dd3da2.sql"))
+        assert os.path.exists("migrations/000_none-628c46f278dd3da2.sql")
 
         with open("schema.sql", "w") as file:
             file.write("CREATE TABLE users (id INTEGER);\n")
@@ -88,12 +78,9 @@ class CmdGenerateMigrationsTestCase(BaseTestCase):
         self.repo.index.commit(".")
 
         cmd_generate_migration("schema.sql", "migrations")
-        self.assertTrue("Schema file name: schema.sql" in self.stdout.getvalue())
+        assert "Schema file name: schema.sql" in self.stdout.getvalue()
 
-        self.assertTrue(
-            os.path.exists("migrations/001_628c46f278dd3da2-c0659d1fe44cd0ef.sql")
-        )
+        assert os.path.exists("migrations/001_628c46f278dd3da2-c0659d1fe44cd0ef.sql")
+
         with open("migrations/001_628c46f278dd3da2-c0659d1fe44cd0ef.sql") as f:
-            self.assertEqual(
-                True, 'alter table "public"."users" drop column "name";' in f.read()
-            )
+            assert 'alter table "public"."users" drop column "name";' in f.read()
--- tests/test_internals.py
+++ tests/test_internals.py
@@ -12,18 +12,16 @@ class InternalsTestCase(BaseTestCase):
         open("migrations/004_none-abc123.sql", "w").close()
         open("migrations/005_abc123-def456.sql", "w").close()
         open("migrations/006_def456-ghi789.sql", "w").close()
-        self.assertEqual("006_def456-ghi789.sql", get_last_migration_file("migrations"))
+        assert get_last_migration_file("migrations") == "006_def456-ghi789.sql"
 
     def test_read_target_fingerprint_from_migration_filename(self):
-        self.assertEqual(
-            "ghi789", get_migration_target_fingerprint("006_def456-ghi789.sql")
-        )
+        assert get_migration_target_fingerprint("006_def456-ghi789.sql") == "ghi789"
 
     def test_get_next_migration_index(self):
         open("migrations/004_none-abc123.sql", "w").close()
         open("migrations/005_abc123-def456.sql", "w").close()
         open("migrations/006_def456-ghi789.sql", "w").close()
-        self.assertEqual(7, get_next_migration_index("migrations"))
+        assert get_next_migration_index("migrations") == 7
 
     def test_get_schema_content_at_fingerprint(self):
         with open("schema.sql", "w") as file:
@@ -45,6 +43,6 @@ class InternalsTestCase(BaseTestCase):
             "schema.sql", "628c46f278dd3da2"
         )
         assert first_schema_version != None
-        self.assertIn("users", first_schema_version)
-        self.assertNotIn("widgets", first_schema_version)
-        self.assertNotIn("vehicles", first_schema_version)
+        assert "users" in first_schema_version
+        assert "widgets" not in first_schema_version
+        assert "vehicles" not in first_schema_version
